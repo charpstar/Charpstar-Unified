5e29931f8e577a4dfe83a6d01877501032902624
unknown <xlkarthik@gmail.com>
2025-09-16 10:07:00 +0530

    Test

--- app/(dashboard)/asset-library/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { AssetLibrarySkeleton } from "@/components/ui/skeletons";
-
-export default function AssetLibraryLoading() {
-  return <AssetLibrarySkeleton />;
-}
--- components/invoice/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as InvoiceGenerator } from "./InvoiceGenerator";
